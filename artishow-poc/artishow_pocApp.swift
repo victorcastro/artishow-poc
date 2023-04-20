8255fee368e3b10a1ef07529c88769fac586b407
Victor Hugo Castro Contreras <victor.castro.c@scotiabank.com.pe>
2023-04-19 20:44:40 -0500

    Initial Commit

---
+++ artishow-poc/artishow_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  artishow_pocApp.swift
+//  artishow-poc
+//
+//  Created by Victor Castro on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct artishow_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
